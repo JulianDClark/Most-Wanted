10d7baaa70a9319ac30ca4fd1315d1c26109b197
Jeanking23 <jeandedieu405@gmail.com>
2023-03-28 08:45:37 -0400

    as a user I want to be able to look up someone's information after I find them with the program display the person's full information

--- MostWanted_FSF_JS_Starter-main/app.js
+++ MostWanted_FSF_JS_Starter-main/app.js
@@ -38,73 +38,71 @@ function searchPeopleDataSet(people) {
         case 'traits':
 
             //! TODO
-            let traitResults = people.map(person => {
-                    return person.traits;
-                }
-                results = searchByTraits(people);
+            return people.filter(person => person[trait] === searchValue);
+            // results = searchByTraits(people);
 
-                break;
+            break;
 
-                default:
-                return searchPeopleDataSet(people);
-            }
-
-            return people.filter(people => results.includes(people));
+        default:
+            return searchPeopleDataSet(people);
     }
 
-    function searchById(people) {
-        const idToSearchForString = prompt('Please enter the id of the person you are searching for.');
-        const idToSearchForInt = parseInt(idToSearchForString);
-        const idFilterResults = people.filter(person => person.id === idToSearchForInt);
-        return idFilterResults;
+    return people.filter(people => results.includes(people));
+}
+
+function searchById(people) {
+    const idToSearchForString = prompt('Please enter the id of the person you are searching for.');
+    const idToSearchForInt = parseInt(idToSearchForString);
+    const idFilterResults = people.filter(person => person.id === idToSearchForInt);
+    return idFilterResults;
+}
+
+function searchByName(people) {
+    const firstNameToSearchFor = prompt('Please enter the the first name of the person you are searching for.');
+    const lastNameToSearchFor = prompt('Please enter the the last name of the person you are searching for.');
+    const fullNameSearchResults = people.filter(person => (person.firstName.toLowerCase() === firstNameToSearchFor.toLowerCase() && person.lastName.toLowerCase() === lastNameToSearchFor.toLowerCase()));
+    return fullNameSearchResults;
+}
+
+function mainMenu(person, people) {
+
+    const mainMenuUserActionChoice = validatedPrompt(
+        `Person: ${person.firstName} ${person.lastName}\n\nDo you want to know their full information, family, or descendants?`, ['info', 'family', 'descendants', 'quit']
+    );
+
+    switch (mainMenuUserActionChoice) {
+        case "info":
+            //! TODO
+            displayPersonInfo(person);
+            break;
+        case "family":
+            //! TODO
+            let personFamily = findPersonFamily(person, people);
+            displayPeople('Family', personFamily);
+            break;
+        case "descendants":
+            //! TODO
+            let personDescendants = findPersonDescendants(person, people);
+            displayPeople('Descendants', personDescendants);
+            break;
+        case "quit":
+            return;
+        default:
+            alert('Invalid input. Please try again.');
     }
 
-    function searchByName(people) {
-        const firstNameToSearchFor = prompt('Please enter the the first name of the person you are searching for.');
-        const lastNameToSearchFor = prompt('Please enter the the last name of the person you are searching for.');
-        const fullNameSearchResults = people.filter(person => (person.firstName.toLowerCase() === firstNameToSearchFor.toLowerCase() && person.lastName.toLowerCase() === lastNameToSearchFor.toLowerCase()));
-        return fullNameSearchResults;
-    }
+    return mainMenu(person, people);
+}
 
-    function mainMenu(person, people) {
+function displayPeople(displayTitle, peopleToDisplay) {
+    const formatedPeopleDisplayText = peopleToDisplay.map(person => `${person.firstName} ${person.lastName}`).join('\n');
+    alert(`${displayTitle}\n\n${formatedPeopleDisplayText}`);
+}
 
-        const mainMenuUserActionChoice = validatedPrompt(
-            `Person: ${person.firstName} ${person.lastName}\n\nDo you want to know their full information, family, or descendants?`, ['info', 'family', 'descendants', 'quit']
-        );
+function validatedPrompt(message, acceptableAnswers) {
+    acceptableAnswers = acceptableAnswers.map(aa => aa.toLowerCase());
 
-        switch (mainMenuUserActionChoice) {
-            case "info":
-                //! TODO
-                displayPersonInfo(person);
-                break;
-            case "family":
-                //! TODO
-                let personFamily = findPersonFamily(person, people);
-                displayPeople('Family', personFamily);
-                break;
-            case "descendants":
-                //! TODO
-                let personDescendants = findPersonDescendants(person, people);
-                displayPeople('Descendants', personDescendants);
-                break;
-            case "quit":
-                return;
-            default:
-                alert('Invalid input. Please try again.');
-        }
-
-        return mainMenu(person, people);
-    }
-
-    function displayPeople(displayTitle, peopleToDisplay) {
-        const formatedPeopleDisplayText = peopleToDisplay.map(person => `${person.firstName} ${person.lastName}`).join('\n');
-        alert(`${displayTitle}\n\n${formatedPeopleDisplayText}`);
-    }
-
-    function validatedPrompt(message, acceptableAnswers) {
-        acceptableAnswers = acceptableAnswers.map(aa => aa.toLowerCase());
-
-        const builtPromptWithAcceptableAnswers = `${message} \nAcceptable Answers: ${acceptableAnswers.map(aa => `\n-> ${aa}`).join('')}`;
+    const builtPromptWithAcceptableAnswers = `${message} \nAcceptable Answers: ${acceptableAnswers.map(aa => `\n-> ${aa}`).join('')}`;
 
     const userResponse = prompt(builtPromptWithAcceptableAnswers).toLowerCase();
 
